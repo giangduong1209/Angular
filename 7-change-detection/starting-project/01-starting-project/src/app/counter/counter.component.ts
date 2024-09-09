a8b275f4bcff33c6cbadbceb1d31267b29112a7d
giangduong1209 <giangduong120900@gmail.com>
2024-09-09 19:11:25 +0700

    7-change-detection

---
+++ 7-change-detection/starting-project/01-starting-project/src/app/counter/counter.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
+
+import { InfoMessageComponent } from '../info-message/info-message.component';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+  imports: [InfoMessageComponent],
+})
+export class CounterComponent implements OnInit {
+  count = signal(0);
+  private zone = inject(NgZone);
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('timer expried');
+      }, 5000);
+    });
+  }
+
+  get debugOutput() {
+    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    return 'Counter Component Debug Output';
+  }
+
+  onDecrement() {
+    this.count.update((prevCount) => prevCount - 1);
+  }
+
+  onIncrement() {
+    this.count.update((prevCount) => prevCount + 1);
+  }
+}
